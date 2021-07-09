68e7d11c1f2b6dbacdabe711ad4eb46a4545d37b
Nguyễn Văn Phú <nguyenvanphu@MacBook-Pro-cua-Nguyen.local>
2021-07-10 00:49:35 +0700

    add slide and fix code

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log('Zill proplayer')
+//scroll change bg header
+const mastheadMain = document.querySelector('.masthead');
+const nav = document.querySelector('nav');
+window.addEventListener('scroll',function(){
+    const scrollTop = document.querySelector('html').scrollTop;
+   if(scrollTop > mastheadMain.offsetHeight - nav.offsetHeight)
+   {
+       nav.style.background = "#A9A9A9";
+
+   }
+   else{
+       nav.style.background ="transparent";
+   
+   }
+})
